e70b3ce5d43f1a9aa4a6688c033f5b5e67899d69
unknown <v.sasikala.sasikala@gmail.com>
2021-05-06 19:01:17 +0530

    9

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function togglemenu(){
     var x = document.getElementById("nav");
+    var y = document.getElementById("bannerimgdiv");
     if(x.style.display=="none"){
         x.style.display="block";
+        y.style.marginTop ="350px";
     }
     else{
         x.style.display="none";
+        y.style.marginTop ="17px";
     }
 
  }
