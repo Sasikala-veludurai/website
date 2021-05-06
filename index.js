3ac0476e4bc4683be049ba406816b8716c9a42c8
unknown <v.sasikala.sasikala@gmail.com>
2021-05-06 14:13:51 +0530

    upload1

---
+++ index.js
@@ -0,0 +1,10 @@
+function togglemenu(){
+    var x = document.getElementById("nav");
+    if(x.style.display=="none"){
+        x.style.display="block";
+    }
+    else{
+        x.style.display="none";
+    }
+
+ }
